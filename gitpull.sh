b406211ac703fa330326d90ac18f9a24d7ffb575
kazawan <55086403@qq.com>
2023-11-29 19:28:05 +0800

    modified:   gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #. /etc/profile
+
+
+
+
 echo " ========================== " >> ~/wk/gitlog.txt
 time=$(date "+%Y-%m-%d %H:%M:%S")
 cd `dirname $0` || exit 1 
 
-cd /home/ubuntu/wk/linux 
+cd /home/$(whoami)/wk/linux 
 echo "${time}: == > " >> ~/wk/gitlog.txt
 /usr/bin/git pull >> ~/wk/gitlog.txt 
 
-cd /home/ubuntu/wk/
+cd /home/$(whoami)/wk/
 
 python3.11 ./sidebuilder.py -i linux -overwrite
 
-echo "${time}:执行完成" >> ~/wk/wklog.txt
+echo "${time}:执行完成" >> ~/wk/gitlog.txt
